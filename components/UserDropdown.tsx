98e1d861418d6bcd3aa1222695beed3379a9aa0b
lf21aaj <lf21aaj@herts.ac.uk>
2025-11-27 11:04:52 +0000

    Finished implementation of user dropdown menu with UI primitives (avatar, dropdown) and nav constants, bug fixes, changes. Fixed errors: - constants.ts: strengthened types for nav items - div classname 'md-flex' -> 'md:flex' Changes: - changed dummy name to full name - changed dummy email to university email instead of personal

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -12,6 +12,8 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 import {useRouter} from "next/navigation";
 import {Button} from "@/components/ui/button";
+import {LogOut} from "lucide-react";
+import NavItems from "@/components/NavItems";
 
 const UserDropdown = () => {
     const router = useRouter();
@@ -22,7 +24,7 @@ const UserDropdown = () => {
     }
 
     // dummy user object
-    const user = { name: 'Lynden', email: 'lyndenfenn@gmail.com' };
+    const user = { name: 'Lynden Fenn', email: 'lf21aaj@herts.ac.uk' };
 
     return (
         <DropdownMenu>
@@ -35,7 +37,7 @@ const UserDropdown = () => {
                             {user.name[0]}
                         </AvatarFallback>
                     </Avatar>
-                    <div className="hidden md-flex flex-col items-start">
+                    <div className="hidden md:flex flex-col items-start">
                         <span className='text-base font-medium text-gray-400'>
                             {user.name}
                         </span>
@@ -43,21 +45,34 @@ const UserDropdown = () => {
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="text-gray-400">
-                <div className="flex items-center gap-3 py-2">
-                    <Avatar className="h-8 w-8">
-                        {/* my GitHub profile picture*/}
-                        <AvatarImage src="https://avatars.githubusercontent.com/u/145921468?s=400&u=8ef8a9188288375b8853bf661f06be7ff5c22278&v=4" />
-                        <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
-                            {user.name[0]}
-                        </AvatarFallback>
-                    </Avatar>
-                </div>
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Profile</DropdownMenuItem>
-                <DropdownMenuItem>Billing</DropdownMenuItem>
-                <DropdownMenuItem>Team</DropdownMenuItem>
-                <DropdownMenuItem>Subscription</DropdownMenuItem>
+                <DropdownMenuLabel>
+                    <div className="flex items-center gap-3 py-2">
+                        <Avatar className="h-8 w-8">
+                            {/* my GitHub profile picture*/}
+                            <AvatarImage src="https://avatars.githubusercontent.com/u/145921468?s=400&u=8ef8a9188288375b8853bf661f06be7ff5c22278&v=4" />
+                            <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
+                                {user.name[0]}
+                            </AvatarFallback>
+                        </Avatar>
+                        <div className="hidden md:flex flex-col items-start">
+                        <span className='text-base font-medium text-gray-400'>
+                            {user.name}
+                        </span>
+                            <span className="text-sm text-gray-500">
+                            {user.email}
+                        </span>
+                        </div>
+                    </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator className="bg-gray-600" />
+                <DropdownMenuItem onClick={handleSignOut} className="text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer">
+                    <LogOut className="h-4 w-4 mr-2 hidden sm:block" />
+                    Logout
+                </DropdownMenuItem>
+                <DropdownMenuSeparator className="hidden sm:block bg-gray-600" />
+                <nav className="sm:hidden">
+                    <NavItems />
+                </nav>
             </DropdownMenuContent>
         </DropdownMenu>
     )
--- lib/constants.ts
+++ lib/constants.ts
@@ -2,4 +2,6 @@ export const NAV_ITEMS = [
     { href: '/', label: 'Dashboard' },
     { href: '/search', label: 'Search' },
     { href: '/watchlist', label: 'Watchlist' },
-]
+] as const
+
+export type NavItem = typeof NAV_ITEMS[number]
